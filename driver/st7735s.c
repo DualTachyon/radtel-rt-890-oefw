d75dcf7d03617ed4ad1300977f9a82cc4c24691e
Dual Tachyon <N/A>
2023-10-10 18:44:45 +0100

    Oops.

--- driver/st7735s.c
+++ driver/st7735s.c
@@ -92,7 +92,7 @@ void ST7735S_Init(void)
 	gColorBackground = COLOR_BLACK;
 	// Not used?
 	//DAT_20001118 = 0xFFFF;
-	gColorForeground = COLOR_WHITE
+	gColorForeground = COLOR_WHITE;
 
 	gpio_bits_set(GPIOF, GPIO_PINS_0);
 	DELAY_WaitMS(1);
